7e06f807267b2881d63bb61b7978537d709805c8
user <study6819789@gmail.com>
2023-08-13 22:28:08 +0900

    JwtResolver 수정

--- annode/src/main/java/com/ikchi/annode/Resolver/JwtResolver.java
+++ annode/src/main/java/com/ikchi/annode/Resolver/JwtResolver.java
@@ -3,7 +3,7 @@ package com.ikchi.annode.Resolver;
 import com.ikchi.annode.annotation.JwtToUserMail;
 import com.ikchi.annode.security.JwtProvider;
 import com.ikchi.annode.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -11,20 +11,23 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-//  HTTP 요청을 디스패처 서블릿에서 받으면 서블릿은 요청에 해당하는 컨트롤러 메소드를찾는다
-//  이후 핸들러 메소드의 인자를 변환할수있는지 시도하기 위해서 HandlerMethodArgumentResolver 를 확인한다
-//  만약  supportsParameter 에서 인자를 변환이 가능하다고 true를 반환한다면
-//  resolveArgument() 가 실행되고 의도한 값으로 변환처리후 반환값을 핸들러 메소드의 인자로 자동 전달된다
-//  중요!! HandlerMethodArgumentResolver 는 비즈니스 로직을 담당하는것이 아니라 인자변환의 역할을 담당하니 최소한의 변환 로직만 작성하자
-// 참고!! HandlerMethodArgumentResolver를 사용했기에 컨트롤러에서만 사용되며 일반메소드에서 커스텀어노테이션 사용을 하려면 Aop를 사용해야한다
+//  HTTP 요청을 디스패처 서블릿에서 받으면 핸들러 매핑을 통해 핸들러를 찾게되고
+//  핸들러 어댑터를 이어서 찾은뒤 핸들러 어댑터는 요청에 해당하는 컨트롤러 메소드를찾는다
+//  이때 핸들러 메소드의 인자를 변환할수있는지 시도하기 위해서 HandlerMethodArgumentResolver 를 확인한다
+
+
+//  중요!! HandlerMethodArgumentResolver가 맡은 역할은 요청으로 받은 인자에서 변환을 하는 목적이니 최소한의 변환 로직만 작성하자
 @Component
+@RequiredArgsConstructor
 public class JwtResolver implements HandlerMethodArgumentResolver {
 
-    @Autowired
-    private JwtProvider jwtProvider;
-    @Autowired
-    private UserService userService;
 
+    private final JwtProvider jwtProvider;
+
+    private final UserService userService;
+
+    // supportsParameter에서 인자를 변환이 가능하다고 true를 반환한다면
+    // resolveArgument() 가 실행되고 의도한 값으로 변환처리후 반환값을 핸들러 메소드의 인자로 자동 전달된다
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -36,18 +39,16 @@ public class JwtResolver implements HandlerMethodArgumentResolver {
 
     // 요청헤더 Authorization에 있는 jwt를 받아서 토큰을 파싱하고 토큰에 있는 이메일을 반환한다
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+    public String resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
         NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
         String jwt = webRequest.getHeader("Authorization");
 
-        if (jwt != null) {
-            String userMail = jwtProvider.getEmailFromToken(jwt);
-            return userMail;
+        if (jwt == null || jwt.trim().isEmpty()) {
+            throw new IllegalArgumentException("요청 헤더에 JWT 토큰이 없습니다.");
         }
 
-        return null;
-
+        return jwtProvider.getEmailFromToken(jwt);
     }
 
 
